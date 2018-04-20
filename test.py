4297d27940e905989a0d2812b403fa2f6fa86009
Jhonata Quinonez Avila <jquinon1@eafit.edu.co>
2018-04-20 15:52:20 -0500

    Adding image

--- test.py
+++ test.py
@@ -5,24 +5,16 @@ from PIL import Image, ImageDraw
 
 if __name__ == "__main__":
 
-    imageFile='input.jpg'
+    imageFile='input2.jpg'
+    targetFile='input9.jpg'
     originalImage = Image.open(imageFile) # Imagen completa
     client=boto3.client('rekognition','us-west-2')
-   
+
     with open(imageFile, 'rb') as image:
         response = client.detect_faces(Image={'Bytes': image.read()},Attributes=['ALL'])
-    
-    #print(json.dumps(response['FaceDetails'][0], indent=4, sort_keys=True))
+
+
     print('Detected faces for ' + imageFile + ' Size' , len(response['FaceDetails']))
-    
-    #for faceDetail in response['FaceDetails']:
-        
-        
-        #boundingBox = faceDetail['BoundingBox']
-        #print('The detected face is between ' + str(faceDetail['AgeRange']['Low'])
-        #      + ' and ' + str(faceDetail['AgeRange']['High']) + ' years old')
-        #print('Here are the other attributes:')
-        #print(json.dumps(faceDetail, indent=4, sort_keys=True))
 
     for i in range(0,len(response['FaceDetails'])):
         faceDetails = response['FaceDetails'][i]
@@ -39,8 +31,8 @@ if __name__ == "__main__":
         top = faceDetails['BoundingBox']['Top']
         width = faceDetails['BoundingBox']['Width']
 
-        
-        
+
+
         oX, oY = originalImage.size
 
         imageName = "images/" + mayorConfidence['Type'] + ".png"
@@ -68,7 +60,7 @@ if __name__ == "__main__":
 
         originalImage.paste(im,(int(newLeft),int(newTop)),im)
 
-    
+
     originalImage.save("MODIFIED.jpg", "JPEG")
 
     #print(newCoords)
